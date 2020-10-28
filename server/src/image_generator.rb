390f2b0af52215af1dea28733225cb85cd082fbd
Jan Schill <janschill@me.com>
2020-10-28 21:45:33 +0100

    Image generator

--- server/src/image_generator.rb
+++ server/src/image_generator.rb
@@ -16,3 +16,9 @@ class ImageGenerator < Generator
     kit.to_file("#{path}file.jpg")
   end
 end
+=begin
+path = "path/to/html"
+html = File.open(path)
+kit = IMGKit.new(html, quality: 50)
+kit.to_file('answer.jpg')
+end
