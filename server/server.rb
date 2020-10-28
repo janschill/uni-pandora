113064f027546c4df441b634a2cd3309e4225185
Jan Schill <janschill@me.com>
2020-10-28 22:38:33 +0100

    Use symbols

--- server/server.rb
+++ server/server.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'socket'
+require 'byebug'
 require_relative 'src/parser'
 require_relative 'src/router'
 require_relative 'src/util'
@@ -13,17 +14,18 @@ puts "Server started on #{LOCATION}, listening on port: #{PORT}"
 
 def log_request(request:)
   "#{Util.now_iso_8601} "\
-    "#{request[:headers][:"user-agent"]} "\
-    "#{request[:method]} "\
-    "#{request[:path]} "\
-    "#{request[:body]}"
+    "#{request.headers[:"user-agent"]} "\
+    "#{request.method} "\
+    "#{request.path} "\
+    "#{request.body}"
 end
 
 loop do
   client = server.accept
   request = client.readpartial(2048)
   request = Parser.new.read(request: request)
-  body = request[:body]
+  puts request
+  body = request.body
   response = Router.new.respond(request: request)
   # time_now = Util.now_iso_8601
   # path =
