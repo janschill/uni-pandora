f3affba10d52c8e23a3b4379e9e134db42d5796b
Jan Schill <janschill@me.com>
2020-10-30 11:53:10 +0100

    Cleanup byebug

--- server/src/parser/logger.rb
+++ server/src/parser/logger.rb
@@ -5,8 +5,6 @@ module Parser
     def initialize; end
 
     def parse_row(row:)
-      require 'byebug'
-      byebug
       row.split(', ').each do |kv|
         key, value = kv.split(': ')
         a.push({ key.to_sym => value })
