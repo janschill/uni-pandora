c951c513a6c2c9481cb256e668d57fa15c46dcd3
Jan Schill <janschill@me.com>
2020-10-30 11:53:31 +0100

    Different parsing from file or HTTP

--- server/src/logger_request.rb
+++ server/src/logger_request.rb
@@ -5,9 +5,23 @@ require 'date'
 class LoggerRequest
   attr_reader :id, :time, :path
 
-  def initialize(body:)
+  def initialize; end
+
+  def from_logger(row:)
+    @id = row.id.to_i
+    @time = Date.strptime(row.time)
+    @path = row.path
+    self
+  end
+
+  def from_http(body:)
     @id = body[:id].to_i
     @time = Date.strptime(body[:time])
     @path = body[:path]
+    self
+  end
+
+  def to_s
+    "id: #{@id}, time: #{@time}, path: #{@path}"
   end
 end
